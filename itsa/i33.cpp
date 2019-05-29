9167ee0182e45998555c11e83da5194f219cc076
YouYuanHuang <sy2es94098@gmail.com>
2019-05-29 23:04:29 +0800

    add i33.cpp

--- itsa/i33.cpp
+++ itsa/i33.cpp
@@ -6,23 +6,28 @@
 using namespace std;
 int main(){
     int i, sum, space = 0;
-    string str;
-    stringstream ss;
-    getline(cin, str);
-    str=str.insert(str.size(), " ");
-    for( int i = 0 ; i < str.size() ; i++){
-        if(str[i] == ' ')
-            space++;
+    string str = "\n";
+    while(getline(cin, str))
+    {
+        space = 0 ;
+        sum = 0 ;
+        str=str.insert(str.size(), " ");
+        for( int i = 0 ; i < str.size() ; i++){
+            if(str[i] == ' ')
+                space++;
+        }
+        int data[space] = {0};
+        stringstream ss;
+        for( int i = 0 ; i < space ; i++){
+            ss.clear();
+            ss << str;
+            ss >> data[i];
+            sum += data[i];
+        }
+        cout << "Size: " << space << endl;
+        cout << "Average: " << fixed << setprecision(3) << (float)sum/(float)space << endl;
+        str = "\n";
     }
-    int data[space];
-    for( int i = 0 ; i < space ; i++){
-        ss.clear();
-        ss << str;
-        ss >> data[i];
-        sum += data[i];
-    }
-    cout << "Size: " << space << endl;
-    cout << "Average: " << fixed << setprecision(3) << (float)sum/(float)space << endl;
 }
 
     
