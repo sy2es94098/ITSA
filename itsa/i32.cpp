328c5ac12106ca0db8affa553b9bbf12f12bd00e
YouYuanHuang <sy2es94098@gmail.com>
2019-05-29 23:42:11 +0800

    add i32.cpp

--- itsa/i32.cpp
+++ itsa/i32.cpp
@@ -7,15 +7,20 @@ int main(){
     char done;
     int asc, num;
     getline(cin, str);
+    if(str.size() > 100)
+        return 0;
     cin >> num;
+
     for(int i = 0; i < str.size(); i++){
         asc =(int)str[i];
-        if((asc >= 65 && asc <= 90) || (asc >= 97 && asc <= 122)){
+        if((asc >= 65 && asc <= 90) || (asc >= 97 && asc <= 122) || (asc >= 48 && asc <= 57)){
             asc += num;
             if(asc > 90 && asc < 97)
                 asc = asc - 90 + 64 ;
-            if(asc > 122)
+            else if(asc > 122)
                 asc = asc - 122 + 96 ;
+            else if(asc > 57 && asc < 90)
+                asc -= 10 ;
             done = (char)asc;
             cout << done;
             }
