28f9fddb289045739498d72c06d6d601320530f9
YouYuanHuang <sy2es94098@gmail.com>
2019-05-29 23:51:10 +0800

    fix i32.cpp

--- itsa/i32.cpp
+++ itsa/i32.cpp
@@ -13,20 +13,28 @@ int main(){
 
     for(int i = 0; i < str.size(); i++){
         asc =(int)str[i];
-        if((asc >= 65 && asc <= 90) || (asc >= 97 && asc <= 122) || (asc >= 48 && asc <= 57)){
+        if(asc >= 65 && asc <= 90){
             asc += num;
-            if(asc > 90 && asc < 97)
-                asc = asc - 90 + 64 ;
-            else if(asc > 122)
-                asc = asc - 122 + 96 ;
-            else if(asc > 57 && asc < 90)
+            if(asc > 90)
+                asc -= 26;
+        }
+        else if(asc >= 97 && asc <= 122){
+            asc += num;
+            if(asc > 122)
+                asc -= 26 ;
+        }
+        else if(asc > 48 && asc < 57){
+            asc += num;
+            if(asc > 57)
                 asc -= 10 ;
-            done = (char)asc;
-            cout << done;
-            }
+        }
         else
             cout << str[i];
+    done = (char)asc;
+    cout << done;
+
     }
+
     cout << endl;
 }
 
