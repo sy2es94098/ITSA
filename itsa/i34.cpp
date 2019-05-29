a27a9e72064a46f1820ac161b221722ca835214a
YouYuanHuang <sy2es94098@gmail.com>
2019-05-29 20:23:50 +0800

    add i34.cpp

--- itsa/i34.cpp
+++ itsa/i34.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 int main(){
     float data[2], std;
-    cin >> data[0] >> data[1];
-    if(data[1] == 1)
-        std = ( data[0] - 80) * 0.7;
-    else
-        std = ( data[0] - 70) * 0.6;
+    while(cin >> data[0] >> data[1])
+    {
+        if(data[1] == 1)
+            std = ( data[0] - 80) * 0.7;
+        else
+            std = ( data[0] - 70) * 0.6;
     cout << fixed << setprecision(1) << std << endl;
+    }
 
 }
