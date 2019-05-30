09e9c018d4c909c1acbeb6ab5984a7770fa5b2d7
YouYuanHuang <sy2es94098@gmail.com>
2019-05-30 19:26:36 +0800

    add i31.cpp

--- itsa/i31.cpp
+++ itsa/i31.cpp
@@ -5,34 +5,38 @@
 using namespace std;
 int main(){
     int space;
-    stringstream ss;
-    string str;
-    getline(cin, str);
-    str=str.insert(str.size(), " ");
-    for( int i = 0 ; i < str.size() ; i++){
-        if(str[i] == ' ')
-            space++;
-    }
-    int num, sum, tmp, data[space];
-    for( int i = 0 ; i < space ; i++){
-        ss.clear();
-        ss << str;
-        ss >> data[i];
-    }
-    for( int i = 0 ; i < space ; i++){
-        for( int j = i+1 ; j < space ; j++){
-            if(data[i] == data[j])
-                tmp++;
+    string str = "\n";
+    while(getline(cin, str))
+    {
+        space = 0;
+        str=str.insert(str.size(), " ");
+        for( int i = 0 ; i < str.size() ; i++){
+            if(str[i] == ' ')
+                space++;
         }
-        if(tmp > sum ){
-            sum = tmp;
-            num = data[i]; 
+        int num = 0, sum = 0, tmp = 0, data[space] = {0};
+        stringstream ss;
+        for( int i = 0 ; i < space ; i++){
+            ss.clear();
+            ss << str;
+            ss >> data[i];
         }
+        for( int i = 0 ; i < space ; i++){
+            for( int j = i+1 ; j < space ; j++){
+                if(data[i] == data[j])
+                    tmp++;
+            }
+            if(tmp > sum ){
+                sum = tmp;
+                num = data[i]; 
+            }
+        }
+        if( sum > space/2)
+            cout << num << endl;
+        else
+            cout << "NO" << endl;
     }
-    if( sum > space/2)
-        cout << num << endl;
-    else
-        cout << "NO" << endl;
+
 }
 
 
